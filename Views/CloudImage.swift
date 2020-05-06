e7595306309a2149932083a3e9949e3fd5e6e3f2
Kyle Coburn <kylecoburn@gmail.com>
2020-05-06 13:14:41 -0700

    ♻️load image based on status

--- Views/CloudImage.swift
+++ Views/CloudImage.swift
@@ -5,14 +5,14 @@ private enum CloudImageStatus {
 	case loading, success, error
 }
 
-private func loadImage(_ url: URL) -> UIImage? {
+private func getStatus(_ url: URL) -> CloudImageStatus {
 	do {
 		let resources = try url.resourceValues(forKeys: [.ubiquitousItemDownloadingStatusKey, .ubiquitousItemDownloadingErrorKey, .ubiquitousItemIsDownloadingKey, .ubiquitousItemDownloadRequestedKey])
 		if let status = resources.ubiquitousItemDownloadingStatus {
 			switch status {
 			case .current:
 				if resources.ubiquitousItemIsDownloading == false {
-					return UIImage(contentsOfFile: url.path)
+					return .success
 				}
 			case .notDownloaded:
 				if resources.ubiquitousItemDownloadRequested == false {
@@ -24,56 +24,50 @@ private func loadImage(_ url: URL) -> UIImage? {
 		}
 	} catch {
 		print("resourceValues", error.localizedDescription)
+		return .error
 	}
-	return nil
+	return .loading
 }
 
 struct CloudImage: View {
 	let url: URL?
 	let size: CGFloat
 
-	@State private var image: UIImage?
 	@State private var status: CloudImageStatus
 
 	init(_ url: URL?, size: CGFloat) {
 		let initialStatus: CloudImageStatus
-		let image: UIImage?
 		if let url = url {
 			let inCloud = url.lastPathComponent.hasSuffix(".icloud")
 			if inCloud {
 				let imageFileName = String(url.lastPathComponent.dropFirst().dropLast(6))
 				self.url = url.deletingLastPathComponent().appendingPathComponent(imageFileName)
-				image = nil
 				initialStatus = .loading
 				try? FileManager.default.startDownloadingUbiquitousItem(at: url)
 			} else {
 				self.url = url
-				image = loadImage(url)
-				initialStatus = image != nil ? .success : .loading
+				initialStatus = getStatus(url)
 			}
 		} else {
 			self.url = url
-			image = nil
 			initialStatus = .error
 		}
 		self.size = size
-		self._image = State(initialValue: image)
 		self._status = State(initialValue: initialStatus)
 	}
 
 	var body: some View {
 		Group {
 			if status == .success {
-				Image(uiImage: image!)
+				Image(uiImage: UIImage(contentsOfFile: url!.path)!)
 					.resizable()
 					.aspectRatio(contentMode: .fill)
 					.frame(width: size, height: size)
 					.clipped()
 			} else if status == .loading {
 				LoadingCloudImage(status: status, size: size) { _ in
-					if let url = self.url, let image = loadImage(url) {
-						self.image = image
-						self.status = .success
+					if let url = self.url {
+						self.status = getStatus(url)
 					}
 				}
 			} else if status == .error {
