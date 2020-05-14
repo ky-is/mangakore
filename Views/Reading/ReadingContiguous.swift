6e89ab356728c6341ae03b1c4a0d579cc366af52
Kyle Coburn <kylecoburn@gmail.com>
2020-05-14 11:29:55 -0700

    ♻️improve page calculation logic

--- Views/Reading/ReadingContiguous.swift
+++ Views/Reading/ReadingContiguous.swift
@@ -3,7 +3,9 @@ import SwiftUI
 struct ReadingContiguous: View {
 	let pageURLs: [URL]
 	@ObservedObject var progress: WorkProgress
-	let geometry: GeometryProxy
+
+	let screenWidth: CGFloat
+	let screenHeight: CGFloat
 	@Binding var hasInteracted: Bool
 
 	@State private var savedOffset: CGFloat = 0
@@ -17,7 +19,8 @@ struct ReadingContiguous: View {
 	init(pages: [URL], progress: WorkProgress, geometry: GeometryProxy, hasInteracted: Binding<Bool>) {
 		self.pageURLs = pages
 		self.progress = progress
-		self.geometry = geometry
+		self.screenWidth = geometry.size.width
+		self.screenHeight = geometry.size.height
 		self._hasInteracted = hasInteracted
 		updatePages(update: false)
 		progress.currentVolume.cache(true)
@@ -40,54 +43,53 @@ struct ReadingContiguous: View {
 	}
 
 	var body: some View {
-		let initialOffset = page0Data.url == nil ? -geometry.size.height : 0
-		let screenHeight = geometry.size.height
+		let page0Height = page0Data.image?.height(scaledWidth: screenWidth) ?? screenHeight
+		let scrollOffset = savedOffset + dragOffset + internalOffset - page0Height
+		let isFirstPage = progress.page == 1
+		let isLastPage = progress.page == progress.currentVolume.pageCount
 		return VStack(spacing: 0) {
-			if progress.page > 1 {
-				CloudImage(page0Data, contentMode: .fill, defaultHeight: screenHeight)
+			if isFirstPage {
+				AdvancePage(label: progress.volume > 1 ? "前章" : "未読", alignment: .bottom, height: screenHeight)
 			} else {
-				AdvancePage(label: progress.volume > 1 ? "前章" : "未読", alignment: .bottom, geometry: geometry)
+				CloudImage(page0Data, contentMode: .fill, defaultHeight: screenHeight)
 			}
 			CloudImage(page1Data, contentMode: .fill, defaultHeight: screenHeight)
-			if progress.page < progress.currentVolume.images.count {
+			if !isLastPage {
 				CloudImage(page2Data, contentMode: .fill, defaultHeight: screenHeight)
 			} else {
-				AdvancePage(label: progress.volume < progress.work.volumes.count ? "次章" : "読破", alignment: .top, geometry: geometry)
+				AdvancePage(label: progress.volume < progress.work.volumes.count ? "次章" : "読破", alignment: .top, height: screenHeight)
 			}
 		}
-			.offset(y: savedOffset + dragOffset + initialOffset + internalOffset)
-			.frame(width: geometry.size.width, height: screenHeight, alignment: .top)
+			.offset(y: scrollOffset)
+			.frame(width: screenWidth, height: screenHeight, alignment: .top)
 			.contentShape(Rectangle())
 			.gesture(
-				DragGesture()
+				DragGesture(minimumDistance: 1)
 					.updating($dragOffset) { value, state, transaction in
 						state = self.getScroll(from: value.translation)
 					}
 					.onEnded { value in
 						let newOffset = self.getScroll(from: value.translation)
 						self.savedOffset += newOffset
-						let distance = -(self.savedOffset + self.dragOffset + initialOffset + self.internalOffset)
+						let distance = -(scrollOffset + newOffset) - self.screenHeight
 						if newOffset > 0 { // Scrolled up
-							let willAdvanceVolume = self.progress.page <= 1
-							let threshold = willAdvanceVolume ? screenHeight / 2 : screenHeight
+							let willAdvanceVolume = isFirstPage
+							let threshold = willAdvanceVolume ? -self.screenHeight / 2 : self.screenHeight / 2
 							if distance < threshold {
 								self.progress.advancePage(forward: false)
 								if !willAdvanceVolume {
+									self.internalOffset -= page0Height
 									self.updatePages(update: true)
-									if let newPage0Height = self.page0Data.image?.height(scaledWidth: self.geometry.size.width) {
-										self.internalOffset -= newPage0Height
-									}
 								}
 							}
 						} else { // Scrolled down
-							if let page1Height = self.page1Data.image?.height(scaledWidth: self.geometry.size.width) {
-								let page0Height = self.page0Data.image?.height(scaledWidth: self.geometry.size.width) ?? screenHeight
-								let willAdvanceVolume = self.progress.page >= self.progress.currentVolume.pageCount
-								let threshold = page0Height + (willAdvanceVolume ? page1Height - screenHeight / 2 : page1Height / 2)
+							if let page1Height = self.page1Data.image?.height(scaledWidth: self.screenWidth) {
+								let willAdvanceVolume = isLastPage
+								let threshold = (willAdvanceVolume ? page0Height - self.screenHeight / 2 : page0Height) + page1Height / 2
 								if distance > threshold {
 									self.progress.advancePage(forward: true)
 									if !willAdvanceVolume {
-										self.internalOffset += page0Height + initialOffset
+										self.internalOffset += page1Height
 										self.updatePages(update: true)
 									}
 								}
@@ -105,15 +107,15 @@ struct ReadingContiguous: View {
 private struct AdvancePage: View {
 	let label: String
 	let alignment: Alignment
-	let geometry: GeometryProxy
+	let height: CGFloat
 
 	var body: some View {
 		ZStack {
 			Text(label)
 				.font(Font.title.bold())
-				.frame(height: geometry.size.height / 2, alignment: alignment)
+				.frame(height: height / 2, alignment: alignment)
 		}
-			.frame(height: geometry.size.height)
+			.frame(height: height)
 	}
 }
 
